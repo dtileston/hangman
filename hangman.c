04040bedb927ab01eb4bfd22bb088e6ab6adef4f
Dave Tileston <dtileston@gmail.com>
2015-11-13 13:44:32 -0500

    More file error handling for stats file  - consolidated some code related to finishing the game  - small bug fixes related stat keeping  - ignore .swp files in .gitignore

--- hangman.c
+++ hangman.c
@@ -1,3 +1,4 @@
+#include <curses.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,6 +6,7 @@
 #include <sys/types.h>
 #include <termcap.h>
 #include <time.h>
+#include <unistd.h>
 
 const char *man[] = { " O\n", "/", "|", "\\\n", " /", "\\\n"};
 typedef struct {
@@ -14,38 +16,44 @@ typedef struct {
 } stats;
 
 void get_word(FILE *file, char *secret);
-void readwrite_stats(stats *stats, const char *mode);
+int readwrite_stats(stats *stats, const char *mode);
 void clear_screen();
+int finish_game(stats *stats);
 
 int main(int argc, char **argv)
 {
-	// testing readwrite_stats
-	stats *stats = malloc(sizeof(stats));;
-	readwrite_stats(stats, "r");
-	stats->games++;
-	printf("%i games, %i wins, %i guesses\n", stats->games, stats->wins, stats->guesses);
 	srand(time(NULL));
+	int err = 0;
 	char homedir[50];
 	strncpy(homedir, getenv("HOME"), sizeof(homedir));
 	const char *word_file = argc > 1 ? argv[1] : strncat(homedir, "/.words", sizeof(homedir));
-	printf("using %s\n", word_file); //REMOVE
+	printf("using dictionary file %s\n", word_file);
+
 	FILE *words = fopen(word_file, "r");
 	if(!words) {
 		perror("Problem opening file");
 		return errno;
 	}
 	char secret[35] = {'\0'};
-	int known[35] = {0};
-	int used[26] = {0};
 	get_word(words, secret);
-	if(!strlen(secret)) {
+	int secret_len = strlen(secret);
+	if(!secret_len) {
 		// some error for 0 length word
 		return 1;
 	}
-	fclose(words);
+	if(EOF == fclose(words)) {
+		perror("Error closing dictionary file");
+		return errno;
+	}
 
-	int secret_len = strlen(secret);
+	stats *stats = malloc(sizeof(stats));;
+	err = readwrite_stats(stats, "r");
+	if(err) return err;
+
+	
 	//prep secret
+	int known[35] = {0};
+	int used[26] = {0}; // tracks which letters have been tried
 	for(int i=0; i < secret_len; i++)
 	{
 		if(secret[i] >= 'a' && secret[i] <= 'z') secret[i] -= 32;
@@ -56,7 +64,6 @@ int main(int argc, char **argv)
 	int guesses = 0;
 	while(wrong < 6)
 	{
-		clear_screen();
 		for(int i=0; i < wrong; i++) printf("%s", man[i]);
 		printf("\n");
 		for(int i=0; i < secret_len; i++)
@@ -64,17 +71,25 @@ int main(int argc, char **argv)
 			if(secret[i] == ' ') printf("  ");
 			else printf("%c ", known[i] ? secret[i] : '_');
 		}
+		double avg = stats->games > 0 ? (double)stats->guesses/(double)stats->games : 0;
+		printf("\n%i Game%s\n%i Win%s\n%i Guess%s\n%.1f Avg. Guess%s per Game\n",
+				stats->games, stats->games == 1 ? "" : "s",
+				stats->wins, stats->wins == 1 ? "" : "s",
+				stats->guesses, stats->guesses == 1 ? "": "es",
+				avg, avg < 1 || avg > 1 ? "es": "");
 		printf("\nUsed: ");
 		for(int i=0; i < 26; i++) if(used[i]) printf("%c ", i+65);
 		printf("\nGuess: ");
 		char c = getchar();
+		while('\n' != getchar());
+		clear_screen();
+
 		if(c < 'A' || (c > 'Z' && c < 'a') || c > 'z') continue;
 		guesses++;
+		stats->guesses++;
 		c -= (c >= 'a' && c <= 'z') ? 32 : 0; // Capitalize the guess
-		used[c-65]++;
-		char n;
-		while('\n' != (n=getchar()));
-		int flag = 0;
+		used[c-'A']++;
+				int flag = 0;
 		for(int i=0; i < secret_len; i++) {
 			flag += (c == secret[i]);
 			known[i] += (c == secret[i]);
@@ -95,13 +110,13 @@ int main(int argc, char **argv)
 			}
 			printf("\nYou win!\n");
 			stats->wins++;
-			return 0;
+			stats->games++;
+			return finish_game(stats);
 		}
-		// update stats
-		stats->guesses += guesses;
-		readwrite_stats(stats, "w");
 	}
 	printf("You lose! The answer was %s\n", secret);
+	stats->games++;
+	return finish_game(stats);
 }
 
 void get_word(FILE *file, char *secret)
@@ -123,10 +138,7 @@ void get_word(FILE *file, char *secret)
 		int i;
 		for(i=0; i < (int)strlen(word); i++)
 		{
-			if((word[i] != ' ' && word[i] < 'A') ||
-			   (word[i] > 'Z' && word[i] < 'a') ||
-				word[i] > 'z')
-			{
+			if((word[i] != ' ' && word[i] < 'A') || (word[i] > 'Z' && word[i] < 'a') || word[i] > 'z') {
 				break;
 			}
 		}
@@ -141,20 +153,62 @@ void get_word(FILE *file, char *secret)
  *
  * function accepts pointer to stats structure and a file mode
  * mode can be either "r" or "w"
+ *
+ * returns int
+ *  < 0: system call error
+ *    0: no error
+ *  > 0: other error
  */
-void readwrite_stats(stats *stats, const char *mode)
+int readwrite_stats(stats *stats, const char *mode)
 {
-	char statfilename[50];;
+	int err = 0;
+	char statfilename[50];
 	strncpy(statfilename, getenv("HOME"), sizeof(statfilename));
 	strncat(statfilename, "/.hangman", sizeof(statfilename));
-	printf("file %s\n", statfilename);
-	FILE *statfile = fopen(statfilename, mode);
+	FILE *statfile;
 	if(mode[0] == 'r') {
-		fscanf(statfile, "%i %i %i", &(stats->games), &(stats->wins), &(stats->guesses));
+		if(access(statfilename, F_OK) == -1) { // if the file doesn't exist
+			statfile = fopen(statfilename, "w+");
+			if(!statfile) {
+				perror("Failed to create new ~/.hangman file.");
+				return errno; //dont try to close
+			} else {
+				if(0 > fprintf(statfile, "0 0 0"))	{
+					perror("Output error occurred when initializing new ~/.hangman file.");
+					err = errno;
+				}
+			}
+			stats->games = stats->wins = stats->guesses = 0;
+		} else { // file already exists
+			statfile = fopen(statfilename, mode);
+			if(!statfile) {
+				perror("Failed to open ~/.hangman for reading.");
+				return errno; //dont try to close
+			} else if (EOF == fscanf(statfile, "%i %i %i", &(stats->games), &(stats->wins), &(stats->guesses))) {
+				perror("Error reading from ~/.hangman. File may be corrupt.");
+				err = errno;
+			}
+		}
+	} else if (mode[0] == 'w') {
+		statfile = fopen(statfilename, mode);
+		if(!statfile) {
+			perror("Failed to open ~/.hangman for writing");
+			return errno; //dont try to close
+		}
+		
+		if(0 > fprintf(statfile, "%i %i %i", stats->games, stats->wins, stats->guesses))	{
+			perror("Output error occurred when writing to ~/.hangman file.");
+			err = errno;
+		}
 	} else {
-		fprintf(statfile, "%i %i %i", stats->games, stats->wins, stats->guesses);
+		perror("Unknown file mode passed to readwrite_stats().");
+		return 1;
 	}
-	fclose(statfile);
+	if(EOF == fclose(statfile))	{
+		perror("Error closing ~/.hangman file.");
+		err = errno;
+	}
+	return err;
 }
 
 /* Retrieved from: http://stackoverflow.com/a/17271828
@@ -169,3 +223,11 @@ void clear_screen()
 	str = tgetstr(cl, NULL);
 	fputs(str, stdout);
 } 
+
+int finish_game(stats *stats)
+{
+	int err = readwrite_stats(stats, "w");
+	if(!err) return err;
+	free(stats);
+	return 0;
+}
